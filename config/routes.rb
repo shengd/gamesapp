36ea888d507b45ee42081f8f280142a621580c38
dsheng <dsheng@utexas.edu>
2012-03-18 22:41:20 -0500

    Prettier Rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Gamesapp::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
 
-  get "static_pages/about"
+  match '/about', to: 'static_pages#about'
+
+  match '/contact', to: 'static_pages#contact'
+
+#  get "static_pages/home"
+
+#  get "static_pages/help"
+
+#  get "static_pages/about"
+
+#  get "static_pages/contact"
 
   resources :games
 
