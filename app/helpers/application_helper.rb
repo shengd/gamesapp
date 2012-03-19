0ad6d569fbaa2d73882c5902724b8cc0eaf01408
dsheng <dsheng@utexas.edu>
2012-03-18 22:26:08 -0500

    CSS, SASS, views, and routing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Returns the title on a per-page basis
+  def get_title(page_title)
+    title = "Gamesapp"
+    return title if page_title.empty?
+    "#{title} | #{page_title}"
+  end
 end
